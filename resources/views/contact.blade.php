721c310f8ff4e1e0cfb5a07638c2f33c2b5d7c0f
acerjak <62491401+acerjak@users.noreply.github.com>
2020-10-23 15:42:39 -0700

    updated structure of form

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,9 +6,11 @@
     <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body class="bg-gray-100 flex items-center justify-center h-full">
-    <form method="POST" action="/contact"
-          class="bg-white p-6 rounded shadow-md"
-          style="width: 300px">
+    <form 
+        method="POST" 
+        action="/contact"
+        class="bg-white p-6 rounded shadow-md"
+        style="width: 300px">
         @csrf
         <div class="mb-5">
             <label for="email"
