0c564e9a5c532558e16c81276aafed82646eded9
acerjak <62491401+acerjak@users.noreply.github.com>
2020-10-23 16:04:38 -0700

    checks for flash message to show that the message was sent

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -14,14 +14,34 @@
         @csrf
         <div class="mb-5">
             <label for="email"
-                   class="block text-xs uppercase font-semibold mb-1"
-            >Email Address</label>
-            <input id="email" type="text"  name="email"
+                   class="block text-xs uppercase font-semibold mb-1">
+                   Email Address
+            </label>
+
+
+            <input id="email" 
+                   type="text"  
+                   name="email"
                    class="border px-2 py-1 text-sm w-full">
+
+            @error('email')    
+                <div class="text-red-500 text-xs">{{ $message }}</div>   
+            @enderror    
         </div>
-        <button type="submit" 
-                class="bg-blue-500 py-2 text-white rounded-full text-sm w-full"
-        >Email Me</button>
+        <button 
+                type="submit" 
+                class="bg-blue-500 py-2 text-white rounded-full text-sm w-full">
+                Email Me
+        </button>
+
+        @if (session('message'))
+            <p class="text-green-500 text-xs mt-2">
+
+                {{ session('message') }}
+
+            </p>
+        @endif
+
     </form>
 </body>
 </html>
